d63baa0d26a5fd0adbe4654c4157409c2c68ba43
TuraevArtyom <artem.turaev@mail.ru>
2018-11-05 05:00:00 +0700

    first

---
+++ src/test/java/ru/lesson1/NumbersTest.java
@@ -0,0 +1,20 @@
+package ru.lesson1;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumbersTest {
+
+    @Test
+    public void sqrt() {
+    }
+
+    @Test
+    public void square() {
+    }
+
+    @Test
+    public void cos() {
+    }
+}
